8cf8301c698c2aea245cfa90d427e3d16d136cdb
Justin <uvatmvf@gmail.com>
2017-07-06 13:08:50 -0600

    Add files via upload

---
+++ AnimationSample/SampleViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Windows;
+
+namespace AnimationSample
+{
+    public class SampleViewModel : NotifyPropertyChangedBase<SampleViewModel>
+    {
+        private bool _alarm;
+        public bool Alarm
+        {
+            get { return _alarm; }
+            set
+            {
+                if (!_alarm.Equals(value))
+                {
+                    _alarm = value;
+                    OnPropertyChanged("Alarm");                    
+                }
+            }
+        }
+
+        private bool _flash;
+        public bool Flash
+        {
+            get { return _flash; }
+            set
+            {
+                if (!_flash.Equals(value))
+                {
+                    _flash = value;
+                    OnPropertyChanged("Flash");
+                }
+            }
+        }
+
+
+        private string _description = "I am an alarm!";
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                if(!_description.Equals(value))
+                {
+                    _description = value;
+                    OnPropertyChanged("Description");
+                }
+            }
+        }
+    }
+
+    public abstract class NotifyPropertyChangedBase<T> : DependencyObject, INotifyPropertyChanged
+    {
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            { PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName)); }
+        }
+
+        protected virtual void OnPropertyChanged<T2>(Expression<Func<T, T2>> accessor)
+        {
+            OnPropertyChanged(PropertyName(accessor));
+        }        
+      
+        public static string PropertyName<T2>(Expression<Func<T, T2>> accessor)
+        {
+            return ((MemberExpression)accessor.Body).Member.Name;
+        }
+    }
+}
