84ec3a55779848cfbeb68b8bea30dec998fcf164
Justin <uvatmvf@gmail.com>
2019-04-30 22:24:08 -0600

    Added user control with property dependency for demo.

---
+++ AnimationSample/MultiSampleViewer.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace AnimationSample
+{
+    /// <summary>
+    /// Interaction logic for MultiSampleViewer.xaml
+    /// </summary>
+    public partial class MultiSampleViewer : UserControl
+    {
+        public MultiSampleViewer()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public int ViewIndex
+        {
+            get { return (int)GetValue(ViewIndexProperty); }
+            set { SetValue(ViewIndexProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ViewIndex.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ViewIndexProperty =
+            DependencyProperty.Register("ViewIndex", typeof(int), typeof(MultiSampleViewer), new PropertyMetadata(0));
+
+
+    }
+}
